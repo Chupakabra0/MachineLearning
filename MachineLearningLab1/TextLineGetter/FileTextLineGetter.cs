d2557f2035585e7da88a3cc75678c3ab8bd7fd60
Сафиюлин Александр <rootalfa4@gmail.com>
2022-10-20 15:33:10 +0300

    First version

---
+++ MachineLearningLab1/TextLineGetter/FileTextLineGetter.cs
@@ -0,0 +1,45 @@
+﻿namespace MachineLearningLab1.TextLineGetter
+{
+    internal class FileTextLineGetter : ITextLineGetter
+    {
+        public FileTextLineGetter(FileInfo fileInfo)
+        {
+            this.Init(fileInfo.OpenRead());
+        }
+
+        public FileTextLineGetter(FileStream fileReadStream)
+        {
+            this.Init(fileReadStream);
+        }
+
+        ~FileTextLineGetter()
+        {
+            this.streamReader_.Close();
+        }
+
+        public string? GetTextLine()
+        {
+            return streamReader_.ReadLine();
+        }
+
+        public void Reset()
+        {
+            streamReader_.DiscardBufferedData();
+            streamReader_.BaseStream.Seek(0, SeekOrigin.Begin);
+        }
+
+        private StreamReader streamReader_;
+
+        private void Init(FileStream fileReadStream)
+        {
+            if (fileReadStream.CanRead)
+            {
+                this.streamReader_ = new StreamReader(fileReadStream);
+            }
+            else
+            {
+                throw new Exception("FileStream opened at not read");
+            }
+        }
+    }
+}
---
+++ MachineLearningLab1/Utils/StringUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace MachineLearningLab1.Utils
+{
+    internal static class StringUtils
+    {
+        public static string GetRidOfRedundantSymbols(this string str) =>
+            // TODO: add removal of one-quotes in such cases like 'fuck', 'camel' etc
+            new(str
+                .Where(c => c == '\'' || char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))
+                .Select(char.ToUpper)
+                .ToArray()
+            );
+    }
+}
