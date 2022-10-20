41e4c6e01e7cf549de05db4d137a8685b7ded902
Сафиюлин Александр <rootalfa4@gmail.com>
2022-10-20 15:07:22 +0300

    Project init

---
+++ MachineLearningLab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MachineLearningLab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
