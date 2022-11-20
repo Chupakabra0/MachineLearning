3b31f679af8c5d1f0511bee4aa3bc55d15a37c85
Сафиюлин Александр <rootalfa4@gmail.com>
2022-11-20 14:56:42 +0200

    Project init

---
+++ MachineLearningLab2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MachineLearningLab2
+{
+    internal class Program
+    { 
+        internal class Options
+        {
+
+        }
+
+        private static void ParseOptionsAndRun(string[] args)
+        {
+            Program.Run(null);
+        }
+
+        private static void Run(Options? options)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Program.ParseOptionsAndRun(args);
+        }
+    }
+}
