5d50eeb31ea1640a778a7044a6f5f393a626b085
Сафиюлин Александр <rootalfa4@gmail.com>
2023-01-05 17:24:06 +0200

    Some argparse improvments

--- MachineLearningLab2/Program.cs
+++ MachineLearningLab2/Program.cs
@@ -1,57 +1,55 @@
-﻿using FLS;
+﻿using CommandLine;
+using CommandLine.Text;
+using FLS;
 using FLS.Rules;
 
 namespace MachineLearningLab2 {
-    internal class Program { 
-        internal class Options {
-
+    internal class Program {
+        internal class TestOptions {
+            [Option('t', "test", SetName = "app", HelpText = "Flag, that launch test cases")]
+            public bool IsTest { get; set; }
         }
 
-        private static void ParseOptionsAndRun(string[] args) {
-            Program.TestRun();
+        internal class ProgramOptions {
+            [Option('c', "cpu", Required = true, HelpText = "CPU performance rate [0; 100]")]
+            public double CPU { get; set; }
+
+            [Option('g', "gpu", Required = true, HelpText = "GPU performance rate [0; 100]")]
+            public double GPU { get; set; }
+
+            [Option('r', "ram", Required = true, HelpText = "RAM performance rate [0; 100]")]
+            public double RAM { get; set; }
         }
 
-        private static void PrintResult(string preString, double result) {
-            Console.WriteLine(preString);
-
-            if (result < 20.0) {
-                Console.WriteLine($"Stupid dirty PC... Rate: {result}");
-            }
-            else if (result < 30.0) {
-                Console.WriteLine($"Slow PC :( Rate: {result}");
-            }
-            else if (result < 65.0) {
-                Console.WriteLine($"Good PC :) Rate: {result}");
-            }
-            else {
-                Console.WriteLine($"Damn son where'd you find this?! Rate: {result}");
-            }
+        public static void Main(string[] args) {
+            Program.ParseOptionsAndRun(args);
         }
 
-        private static void TestRun() {
+        private static IFuzzyEngine CreateFuzzyEngine() {
+            var fuzzyEngine = new FuzzyEngineFactory().Default();
+
             var centralProcessorLingusticVar = new LinguisticVariable("CPU");
-            var centralProcessorBad          = centralProcessorLingusticVar.MembershipFunctions.AddTriangle("Bad CPU", 0, 7.5, 14);
-            var centralProcessorGood         = centralProcessorLingusticVar.MembershipFunctions.AddTriangle("Good CPU", 10, 15, 20);
-            var centralProcessorPerfect      = centralProcessorLingusticVar.MembershipFunctions.AddTriangle("Perfect CPU", 17.5, 40, 100);
+            var centralProcessorBad = centralProcessorLingusticVar.MembershipFunctions.AddTriangle("Bad CPU", 0, 7.5, 14);
+            var centralProcessorGood = centralProcessorLingusticVar.MembershipFunctions.AddTriangle("Good CPU", 10, 15, 20);
+            var centralProcessorPerfect = centralProcessorLingusticVar.MembershipFunctions.AddTriangle("Perfect CPU", 17.5, 40, 100);
 
             var videoCardLingusticVar = new LinguisticVariable("GPU");
-            var videoCardBad          = videoCardLingusticVar.MembershipFunctions.AddTriangle("Bad GPU", 0, 10, 20);
-            var videoCardGood         = videoCardLingusticVar.MembershipFunctions.AddTriangle("Good GPU", 15, 35, 55);
-            var videoCardPerfect      = videoCardLingusticVar.MembershipFunctions.AddTriangle("Perfect GPU", 40, 100, 100);
+            var videoCardBad = videoCardLingusticVar.MembershipFunctions.AddTriangle("Bad GPU", 0, 10, 20);
+            var videoCardGood = videoCardLingusticVar.MembershipFunctions.AddTriangle("Good GPU", 15, 35, 55);
+            var videoCardPerfect = videoCardLingusticVar.MembershipFunctions.AddTriangle("Perfect GPU", 40, 100, 100);
 
             var memoryLingusticVar = new LinguisticVariable("RAM");
-            var memoryBad          = memoryLingusticVar.MembershipFunctions.AddTriangle("Bad RAM", 0, 12.5, 30);
-            var memoryGood         = memoryLingusticVar.MembershipFunctions.AddTriangle("Good RAM", 25, 40, 55);
-            var memoryPerfect      = memoryLingusticVar.MembershipFunctions.AddTriangle("Perfect RAM", 45, 100, 100);
+            var memoryBad = memoryLingusticVar.MembershipFunctions.AddTriangle("Bad RAM", 0, 12.5, 30);
+            var memoryGood = memoryLingusticVar.MembershipFunctions.AddTriangle("Good RAM", 25, 40, 55);
+            var memoryPerfect = memoryLingusticVar.MembershipFunctions.AddTriangle("Perfect RAM", 45, 100, 100);
 
             var computerSpeedLingusticVar = new LinguisticVariable("PC Speed");
-            var computerSpeedBad          = computerSpeedLingusticVar.MembershipFunctions.AddTrapezoid("Bad PC Speed", 0, 0, 15, 30);
-            var computerSpeedGood         = computerSpeedLingusticVar.MembershipFunctions.AddTriangle("Good PC Speed", 20, 50, 75);
-            var computerSpeedPerfect      = computerSpeedLingusticVar.MembershipFunctions.AddTrapezoid("Perfect PC Speed", 65, 85, 100, 100);
+            var computerSpeedBad = computerSpeedLingusticVar.MembershipFunctions.AddTrapezoid("Bad PC Speed", 0, 0, 15, 30);
+            var computerSpeedGood = computerSpeedLingusticVar.MembershipFunctions.AddTriangle("Good PC Speed", 20, 50, 75);
+            var computerSpeedPerfect = computerSpeedLingusticVar.MembershipFunctions.AddTrapezoid("Perfect PC Speed", 65, 85, 100, 100);
 
             var ruleList = new List<FuzzyRule> {
                 // slow CPU cases
-
                 Rule.If (
                     centralProcessorLingusticVar.Is(centralProcessorBad)
                         .And(memoryLingusticVar.Is(memoryBad))
@@ -81,7 +79,6 @@ namespace MachineLearningLab2 {
                 ).Then(computerSpeedLingusticVar.Is(computerSpeedBad)),
 
                 // good CPU cases
-
                 Rule.If (
                     centralProcessorLingusticVar.Is(centralProcessorGood)
                         .And(memoryLingusticVar.Is(memoryPerfect))
@@ -114,7 +111,6 @@ namespace MachineLearningLab2 {
                 ).Then(computerSpeedLingusticVar.Is(computerSpeedBad)),
 
                 // perfect CPU cases
-
                 Rule.If (
                     centralProcessorLingusticVar.Is(centralProcessorPerfect)
                         .And(memoryLingusticVar.Is(memoryGood))
@@ -140,11 +136,42 @@ namespace MachineLearningLab2 {
                 ).Then(computerSpeedLingusticVar.Is(computerSpeedPerfect))
             };
 
-            IFuzzyEngine fuzzyEngine = new FuzzyEngineFactory().Default();
-
             fuzzyEngine.Rules.Add(ruleList.ToArray());
 
-            var result1 = fuzzyEngine.Defuzzify(new {
+            return fuzzyEngine;
+        }
+
+        private static void ParseOptionsAndRun(string[] args) {
+            var parser      = new Parser(with => with.HelpWriter = null);
+            var testOptions = parser.ParseArguments<TestOptions>(args).Value;
+            
+            if (testOptions?.IsTest ?? false) {
+                Program.TestRun();
+            }
+            else {
+                var programOptions = Parser.Default.ParseArguments<ProgramOptions>(args).WithParsed(ArgRun);
+            }
+        }
+
+        private static void PrintResult(string preString, double result) {
+            Console.WriteLine(preString);
+
+            if (result < 20.0) {
+                Console.WriteLine($"Stupid dirty PC... Rate: {result}");
+            }
+            else if (result < 30.0) {
+                Console.WriteLine($"Slow PC :( Rate: {result}");
+            }
+            else if (result < 65.0) {
+                Console.WriteLine($"Good PC :) Rate: {result}");
+            }
+            else {
+                Console.WriteLine($"Damn son where'd you find this?! Rate: {result}");
+            }
+        }
+
+        private static void TestRun() {
+            var result1 = Program.fuzzyEngine.Defuzzify(new {
                 CPU = 13.13,
                 GPU = 17.80,
                 RAM = 30.00
@@ -152,7 +179,7 @@ namespace MachineLearningLab2 {
 
             PrintResult("Ryzen 5 1600 AF + RX 570 + 16 Gb DDR4 2933 MHz CL20", result1);
 
-            var result2 = fuzzyEngine.Defuzzify(new {
+            var result2 = Program.fuzzyEngine.Defuzzify(new {
                 CPU = 19.69,
                 GPU = 42.36,
                 RAM = 75.00
@@ -160,7 +187,7 @@ namespace MachineLearningLab2 {
 
             PrintResult("Ryzen 5 5600G + RTX 2060 + 16 DDR4 3600 MHz CL16", result2);
 
-            var result3 = fuzzyEngine.Defuzzify(new {
+            var result3 = Program.fuzzyEngine.Defuzzify(new {
                 CPU = 37.91,
                 GPU = 68.38,
                 RAM = 95.00
@@ -169,9 +196,20 @@ namespace MachineLearningLab2 {
             PrintResult("Intel i5-13600KF + RTX 3080 12GB + 32 DDR5 6000 MHz CL36", result3);
         }
 
-        public static void Main(string[] args)
-        {
-            Program.ParseOptionsAndRun(args);
+        private static void ArgRun(ProgramOptions options) {
+            double cpu = options.CPU;
+            double gpu = options.GPU;
+            double ram = options.RAM;
+
+            var result = Program.fuzzyEngine.Defuzzify(new {
+                CPU = cpu,
+                GPU = gpu,
+                RAM = ram
+            });
+
+            PrintResult($"CPU: {cpu}, GPU: {gpu}, RAM: {ram}", result);
         }
+
+        private static IFuzzyEngine fuzzyEngine = Program.CreateFuzzyEngine();
     }
 }
