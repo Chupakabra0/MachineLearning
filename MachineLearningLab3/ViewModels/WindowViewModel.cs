86d3534237272004b1eb71d6f9ef039f7d3097b0
Сафиюлин Александр <rootalfa4@gmail.com>
2023-01-09 22:42:50 +0200

    Ok, it works almost perfectly

--- MachineLearningLab3/ViewModels/WindowViewModel.cs
+++ MachineLearningLab3/ViewModels/WindowViewModel.cs
@@ -1,20 +1,42 @@
 ﻿using Craiyon.Net;
 using MachineLearningLab3.Basic;
 using System;
+using System.Collections.ObjectModel;
+using Microsoft.VisualBasic.FileIO;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Wpf.Ui.Appearance;
 
 namespace MachineLearningLab3.ViewModels
 {
     internal class WindowViewModel : BaseViewModel
     {
+        private const int MaxImages          = 9;
+        private const double Scale           = 0.125;
+        private const string ImageFolderName = "images";
+        private const string GenerateText    = "Generate";
+        private const string ProcessingText  = "Generating...";
+
         public WindowViewModel()
         {
-
+            Accent.ApplySystemAccent();
         }
 
-        public ICommand ClickCommand => new RelayCommand(_ => this.GenerateImage(), _ => this.InputText != string.Empty);
+        public ICommand ClickCommand => new RelayCommand(_ => this.GenerateImage(), _ => this.InputText != string.Empty && this.IsWorking == false);
+        public ICommand NextImageCommand => new RelayCommand(_ => this.NextImage(), _ => this.ImageSource != null);
+        public ICommand PrevImageCommand => new RelayCommand(_ => this.PrevImage(), _ => this.ImageSource != null);
+
+        void NextImage()
+        {
+
+            this.Index = this.Index < WindowViewModel.MaxImages - 1 ? this.Index + 1 : 0;
+        }
+
+        void PrevImage()
+        {
+            this.Index = this.Index > 0 ? this.Index - 1 : WindowViewModel.MaxImages - 1;
+        }
 
         async void GenerateImage()
         {
@@ -22,23 +44,47 @@ namespace MachineLearningLab3.ViewModels
 
             try
             {
-                await craiyonService.DownloadGalleryAsync(this.InputText, "testFolder");
+                this.IsWorking  = true;
+                this.ButtonText = WindowViewModel.ProcessingText;
 
+                await craiyonService.DownloadGalleryAsync(this.InputText, $"{WindowViewModel.ImageFolderName}");
+
+                this.IsImageExists         = false;
+                this.ImageSourceCollection = new();
+
+                for (var i = 0; i < WindowViewModel.MaxImages; ++i)
+                {
+                    var newName = $"{Guid.NewGuid()}.jpg";
+                    FileSystem.RenameFile($"{WindowViewModel.ImageFolderName}/{i}.jpg", newName);
+
+                    this.ImageSourceCollection.Add(new TransformedBitmap(new BitmapImage(new Uri($"{WindowViewModel.ImageFolderName}/{newName}", UriKind.Relative)),new ScaleTransform(WindowViewModel.Scale, WindowViewModel.Scale)));
+                }
+
+                this.Index         = 0;
                 this.IsImageExists = true;
-                this.ImageSource   = new TransformedBitmap(
-                    new BitmapImage(
-                        new Uri("testFolder/0.jpg", UriKind.Relative)
-                    ), new ScaleTransform(0.125, 0.125));
+
+                this.OnPropertyChanged(nameof(this.ImageSource));
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
+            finally
+            {
+                this.IsWorking  = false;
+                this.ButtonText = WindowViewModel.GenerateText;
+            }
         }
 
         public string InputText { get; set; } = string.Empty;
-        public ImageSource? ImageSource { get; set; } = null;
+        public string ButtonText { get; set; } = WindowViewModel.GenerateText;
+
+        public ObservableCollection<ImageSource> ImageSourceCollection { get; set; } = new();
+        private int Index { get; set; } = 0;
+        public ImageSource? ImageSource => this.ImageSourceCollection.Count != 0 ?
+            this.ImageSourceCollection?[this.Index] ?? null : null;
 
         public bool IsImageExists { get; set; } = false;
+        public bool IsWorking     { get; set; } = false;
     }
 }
